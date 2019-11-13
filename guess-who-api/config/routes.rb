2ef34df7a7d4dbf0edb854a125bc92353070a755
github username <github email address>
2019-11-13 15:43:24 -0500

    erin wednesday

--- guess-who-api/config/routes.rb
+++ guess-who-api/config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   
   get '/themes', to: "themes#index"
 
+  get '/games', to: "games#index"
+  post '/games', to: "games#create"
+  patch '/games/:id', to: "games#addcard"
+
+
   post '/users/login', to: "users#login"
   get '/cards/:theme_id', to: "cards#filter"
 
