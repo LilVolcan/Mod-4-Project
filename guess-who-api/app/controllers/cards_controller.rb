4e4ef7e82091d23dc7ae8d92ee30a1dced575703
github username <github email address>
2019-11-13 12:36:50 -0500

    udpated controllers

--- guess-who-api/app/controllers/cards_controller.rb
+++ guess-who-api/app/controllers/cards_controller.rb
@@ -1,7 +1,13 @@
 class CardsController < ApplicationController
 
     def index
-    cards = Card.all 
-    render json: CardSerializer.new(cards).serialized_json
+        cards = Card.all 
+        render json: CardSerializer.new(cards).serialized_json
     end
+
+    def filter
+        cards = Card.all.select{ |card| card[:theme_id] == params[:theme_id].to_i}
+        render json: cards
+    end 
+
 end
--- guess-who-api/config/routes.rb
+++ guess-who-api/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :themes
+  # resources :cards
+  # resources :themes, 
   resources :games
   resources :users
+  
+  get '/themes', to: "themes#index"
 
   post '/users/login', to: "users#login"
+  get '/cards/:theme_id', to: "cards#filter"
 
 end
